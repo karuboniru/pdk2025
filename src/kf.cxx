39c61b52410696e5a69ed33ed37357809107265f
Qiyu Yan <yanqiyu17@mails.ucas.ac.cn>
2025-11-27 23:53:04 +0800

    Move Problem6D class definition after SingleKFLagMul template
    The Problem6D class implementation was moved from above the
    SingleKFLagMul template to below it. This reordering improves code
    organization without changing any functionality.

--- src/kf.cxx
+++ src/kf.cxx
@@ -53,85 +53,6 @@ template <size_t N> double square_sum_array(const std::array<double, N> &arr) {
 
 using para_view_t = std::span<const double>;
 
-class Problem6D {
-public:
-  static constexpr size_t kinematic_parameter_count = 6;
-  static constexpr size_t constrain_count = 3;
-  using answer_t = std::array<momentum_t, 2>;
-  Problem6D(const answer_t &measured_) : measured(measured_) {
-    auto gamma_smear_strategy = GetSmearStrategy(22);
-    if (!gamma_smear_strategy) {
-      throw std::runtime_error("No smear strategy for gamma (PDG 22)");
-    }
-    for (size_t i = 0; i < 2; ++i) {
-      double e = measured[i].E();
-      sigma_angle[i] = gamma_smear_strategy->get_sigma_angle(e) * M_PI / 180.0;
-      sigma_momentum[i] = gamma_smear_strategy->get_sigma_energy(e);
-    }
-  }
-
-  std::array<double, constrain_count>
-  get_constrain(const para_view_t &params_kin) const {
-    std::array<double, constrain_count> constrains;
-    auto fitted = get_measured_from_parameters(params_kin);
-    auto pi0_momentum = fitted[0] + fitted[1];
-    constrains[0] = pi0_momentum.M() - 0.134977;
-    auto fitted_pi0_momentum = (fitted[0].Vect() + fitted[1].Vect()).Unit();
-    auto raw_pi0_momentum = (measured[0] + measured[1]).Vect().Unit();
-    constrains[1] = fitted_pi0_momentum.x() - raw_pi0_momentum.x();
-    constrains[2] = fitted_pi0_momentum.y() - raw_pi0_momentum.y();
-    return constrains;
-  }
-
-  double get_parameter_penalty(const para_view_t &params_kin) const {
-    auto fitted = get_measured_from_parameters(params_kin);
-    double penalty = 0.0;
-    for (auto &&[orig, fit, s_ang, s_mom] :
-         std::views::zip(measured, fitted, sigma_angle, sigma_momentum)) {
-      // angle penalty
-      ROOT::Math::XYZVector orig_dir = orig.Vect().Unit();
-      ROOT::Math::XYZVector fitted_dir = fit.Vect().Unit();
-      double angle_diff = std::acos(orig_dir.Dot(fitted_dir));
-      penalty += chi2(angle_diff, 0.0, s_ang);
-
-      // momentum penalty
-      double orig_mom = orig.P();
-      double fitted_mom = fit.P();
-      penalty += chi2(fitted_mom / orig_mom, 1., s_mom);
-    }
-    return penalty;
-  }
-
-  auto generate_initial_parameters(ROOT::Minuit2::MnUserParameters from) const {
-    auto &local_rand = get_thread_local_random();
-    for (size_t i = 0; i < kinematic_parameter_count; ++i) {
-      from.Add(std::format("dx{}", i), local_rand.Gaus(0, 0.05), 0.001);
-    }
-    return from;
-  }
-
-  answer_t get_measured_from_parameters(const para_view_t &params_kin) const {
-    if (params_kin.size() != kinematic_parameter_count) {
-      throw std::runtime_error(
-          "Problem6D::get_measured_from_parameters: invalid parameter size");
-    }
-    answer_t ret;
-    for (auto &&[target, source, p3] :
-         std::views::zip(ret, measured, params_kin | std::views::chunk(3))) {
-      double dx = p3[0];
-      double dy = p3[1];
-      double dz = p3[2];
-      target = change_vector(source, dx, dy, dz);
-    }
-    return ret;
-  }
-
-private:
-  answer_t measured;
-  std::array<double, 2> sigma_angle{};
-  std::array<double, 2> sigma_momentum{};
-};
-
 template <class T>
 class SingleKFLagMul final : public ROOT::Minuit2::FCNBase, public T {
   static constexpr size_t lagrange_parmaeter_count_per_constrain = 2;
@@ -220,6 +141,85 @@ public:
       }
     }
   }
+};
+
+class Problem6D {
+public:
+  static constexpr size_t kinematic_parameter_count = 6;
+  static constexpr size_t constrain_count = 3;
+  using answer_t = std::array<momentum_t, 2>;
+  Problem6D(const answer_t &measured_) : measured(measured_) {
+    auto gamma_smear_strategy = GetSmearStrategy(22);
+    if (!gamma_smear_strategy) {
+      throw std::runtime_error("No smear strategy for gamma (PDG 22)");
+    }
+    for (size_t i = 0; i < 2; ++i) {
+      double e = measured[i].E();
+      sigma_angle[i] = gamma_smear_strategy->get_sigma_angle(e) * M_PI / 180.0;
+      sigma_momentum[i] = gamma_smear_strategy->get_sigma_energy(e);
+    }
+  }
+
+  std::array<double, constrain_count>
+  get_constrain(const para_view_t &params_kin) const {
+    std::array<double, constrain_count> constrains;
+    auto fitted = get_measured_from_parameters(params_kin);
+    auto pi0_momentum = fitted[0] + fitted[1];
+    constrains[0] = pi0_momentum.M() - 0.134977;
+    auto fitted_pi0_momentum = (fitted[0].Vect() + fitted[1].Vect()).Unit();
+    auto raw_pi0_momentum = (measured[0] + measured[1]).Vect().Unit();
+    constrains[1] = fitted_pi0_momentum.x() - raw_pi0_momentum.x();
+    constrains[2] = fitted_pi0_momentum.y() - raw_pi0_momentum.y();
+    return constrains;
+  }
+
+  double get_parameter_penalty(const para_view_t &params_kin) const {
+    auto fitted = get_measured_from_parameters(params_kin);
+    double penalty = 0.0;
+    for (auto &&[orig, fit, s_ang, s_mom] :
+         std::views::zip(measured, fitted, sigma_angle, sigma_momentum)) {
+      // angle penalty
+      ROOT::Math::XYZVector orig_dir = orig.Vect().Unit();
+      ROOT::Math::XYZVector fitted_dir = fit.Vect().Unit();
+      double angle_diff = std::acos(orig_dir.Dot(fitted_dir));
+      penalty += chi2(angle_diff, 0.0, s_ang);
+
+      // momentum penalty
+      double orig_mom = orig.P();
+      double fitted_mom = fit.P();
+      penalty += chi2(fitted_mom / orig_mom, 1., s_mom);
+    }
+    return penalty;
+  }
+
+  auto generate_initial_parameters(ROOT::Minuit2::MnUserParameters from) const {
+    auto &local_rand = get_thread_local_random();
+    for (size_t i = 0; i < kinematic_parameter_count; ++i) {
+      from.Add(std::format("dx{}", i), local_rand.Gaus(0, 0.05), 0.001);
+    }
+    return from;
+  }
+
+  answer_t get_measured_from_parameters(const para_view_t &params_kin) const {
+    if (params_kin.size() != kinematic_parameter_count) {
+      throw std::runtime_error(
+          "Problem6D::get_measured_from_parameters: invalid parameter size");
+    }
+    answer_t ret;
+    for (auto &&[target, source, p3] :
+         std::views::zip(ret, measured, params_kin | std::views::chunk(3))) {
+      double dx = p3[0];
+      double dy = p3[1];
+      double dz = p3[2];
+      target = change_vector(source, dx, dy, dz);
+    }
+    return ret;
+  }
+
+private:
+  answer_t measured;
+  std::array<double, 2> sigma_angle{};
+  std::array<double, 2> sigma_momentum{};
 };
 
 using KFPi0Solver = SingleKFLagMul<Problem6D>;
